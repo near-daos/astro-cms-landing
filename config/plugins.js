ab623c6062d462d4bfb126e5c8533f1364b68b3a
vladimir-ilyashenko <volodymyr.iliashchenko@magicpowered.io>
2022-07-12 22:03:22 +0300

    fixed s3 parameter names

--- config/plugins.js
+++ config/plugins.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        accessKeyId: env('AWS_S3_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_S3_ACCESS_SECRET'),
+        region: env('AWS_S3_REGION'),
         params: {
-            Bucket: env('AWS_BUCKET_NAME'),
+            Bucket: env('AWS_S3_BUCKET_NAME'),
         },
       },
       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
